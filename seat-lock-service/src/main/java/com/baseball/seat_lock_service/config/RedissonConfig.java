1a0e4ced2294fdb087731a3e4c76495b2a7ef54d
minsssssd <mintx0526@naver.com>
2026-05-26 11:48:50 +0900

    서비스별 docker 파일 수정

--- seat-lock-service/src/main/java/com/baseball/seat_lock_service/config/RedissonConfig.java
+++ seat-lock-service/src/main/java/com/baseball/seat_lock_service/config/RedissonConfig.java
@@ -3,15 +3,22 @@ package com.baseball.seat_lock_service.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
         return Redisson.create(config);
     }
 }
